578f8a917dfa82dcaec4ad6c3479016de8dd1188
sgpearse <pearse@ucar.edu>
2026-01-22 10:25:35 -0700

    test /files

--- flask-app/map_api/routes.py
+++ flask-app/map_api/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, render_template, render_template_string, request, send_file
-from .era5_plot import plot_png, NETCDF_FILE, VAR_NAME, TIME_NAME, LAT_NAME, LON_NAME
+#from .era5_plot import plot_png, NETCDF_FILE, VAR_NAME, TIME_NAME, LAT_NAME, LON_NAME
 from pathlib import Path
 import xarray as xr
 import json
@@ -50,41 +50,41 @@ def map_data():
     with open(data_file) as f:
         return jsonify(json.load(f))
 
-@map_blueprint.route("/era5")
-def era5_root():
-    return jsonify(
-        ok=True,
-        message="GET /era5/plot?t=0 returns a PNG map of VAR_2T"
-    )
-
-
-@map_blueprint.route("/era5/info")
-def era5_info():
-    ds = xr.open_dataset(NETCDF_FILE)
-    try:
-        return jsonify({
-            "path": NETCDF_FILE,
-            "variable": VAR_NAME,
-            "time_len": int(ds.dims[TIME_NAME]),
-            "shape": [
-                int(ds.dims[LAT_NAME]),
-                int(ds.dims[LON_NAME])
-            ],
-            "lat_range": [
-                float(ds[LAT_NAME].values.min()),
-                float(ds[LAT_NAME].values.max())
-            ],
-            "lon_range": [
-                float(ds[LON_NAME].values.min()),
-                float(ds[LON_NAME].values.max())
-            ]
-        })
-    finally:
-        ds.close()
-
-@map_blueprint.route("/era5/plot")
-def era5_plot():
-    t = request.args.get("t", default=0, type=int)
-    buf = plot_png(t)
-    return send_file(buf, mimetype="image/png")
-
+#@map_blueprint.route("/era5")
+#def era5_root():
+#    return jsonify(
+#        ok=True,
+#        message="GET /era5/plot?t=0 returns a PNG map of VAR_2T"
+#    )
+#
+#
+#@map_blueprint.route("/era5/info")
+#def era5_info():
+#    ds = xr.open_dataset(NETCDF_FILE)
+#    try:
+#        return jsonify({
+#            "path": NETCDF_FILE,
+#            "variable": VAR_NAME,
+#            "time_len": int(ds.dims[TIME_NAME]),
+#            "shape": [
+#                int(ds.dims[LAT_NAME]),
+#                int(ds.dims[LON_NAME])
+#            ],
+#            "lat_range": [
+#                float(ds[LAT_NAME].values.min()),
+#                float(ds[LAT_NAME].values.max())
+#            ],
+#            "lon_range": [
+#                float(ds[LON_NAME].values.min()),
+#                float(ds[LON_NAME].values.max())
+#            ]
+#        })
+#    finally:
+#        ds.close()
+#
+#@map_blueprint.route("/era5/plot")
+#def era5_plot():
+#    t = request.args.get("t", default=0, type=int)
+#    buf = plot_png(t)
+#    return send_file(buf, mimetype="image/png")
+#
