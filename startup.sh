746577b341c005b3d23264cefc890cc06a891b3d
sgpearse <pearse@ucar.edu>
2026-01-23 14:06:08 -0700

    test

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
+cat startup.sh
 git clone https://github.com/NCAR/docker_map_example.git
-git log -1 --pretty=format:"%h" -- ./docker_map_example
+git -C docker_map_example log -1 --pretty=format:"%h"
 cp -r docker_map_example/flask-app/* .
 python wsgi.py
