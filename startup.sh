1b5975cd7fbcacdf9b55e3297516a9d6d5d18301
sgpearse <pearse@ucar.edu>
2026-01-26 13:55:42 -0700

    show model_predict contents

--- startup.sh
+++ startup.sh
@@ -1,9 +1,5 @@
 ls /output
-cat startup.sh
-pwd
-ls -lrth
 git clone https://github.com/NCAR/docker_map_example.git
 git -C docker_map_example log -1 --pretty=format:"%h"
 cp -r docker_map_example/flask-app/* .
-ls -lrth
 python wsgi.py
