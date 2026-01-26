6c27d8e40cc596a8a7d3351fefe949a151d6277b
sgpearse <pearse@ucar.edu>
2026-01-26 09:30:48 -0700

    test file tree

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 cat startup.sh
+pwd
+ls -lrth
 git clone https://github.com/NCAR/docker_map_example.git
 git -C docker_map_example log -1 --pretty=format:"%h"
 cp -r docker_map_example/flask-app/* .
