9f85e0750ec71bdc2395d75911ff4dc98837132d
sgpearse <pearse@ucar.edu>
2026-01-29 09:53:32 -0700

    add dimension selector

--- flask-app/map_api/era5_plot.py
+++ flask-app/map_api/era5_plot.py
@@ -51,7 +51,7 @@ def plot_png(t: int, var_name: str = VAR_NAME):
             t = int(np.clip(t, 0, da.sizes[TIME_NAME] - 1))
 
             slice2d = da.isel({TIME_NAME: t}).astype("float64")
-            if slice2d.dims().size() > 2:
+            if len(slice2d.dims()) > 2:
                 slice2d = slice2d.isel({LEV_NAME: 0}).astype("float64")
 
             arr = slice2d.values
