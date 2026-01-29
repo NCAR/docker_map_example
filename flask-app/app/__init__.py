7679b8e5559720a350aaa03e997473379c93d4af
sgpearse <pearse@ucar.edu>
2026-01-29 14:22:21 -0700

    move .css

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -5,6 +5,10 @@ app = Flask(
     static_folder="static",
     template_folder="templates")
 
+print("ROOT:", app.root_path)
+print("STATIC:", app.static_folder)
+print("TEMPLATES:", app.template_folder)
+
 from app.main import views
 
 from map_api.routes import map_blueprint
