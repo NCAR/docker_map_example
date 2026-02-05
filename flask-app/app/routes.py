b9c0414255b35ec132d5f2563e1ae1c5ee870e1c
sgpearse <pearse@ucar.edu>
2026-02-05 08:31:22 -0700

    test dataset scan

--- flask-app/app/routes.py
+++ flask-app/app/routes.py
@@ -25,7 +25,7 @@ def scan_datasets():
     data_dir = Path(os.environ.get("MAP_DATA_DIR", "/output/model_predict"))
     print("data dir" + data_dir)
     for d in data_dir.iterdir():
-        print("     dir:" + str(d))
+        print(d)
         if d.is_dir():
             nc_file = d / "*.nc" # Assuming standard naming
             if nc_file.exists():
